b121559214bcc527c0b6b9549b6c365641fd35aa
krivushinme <mkrivushin@yandex.ru>
2009-06-03 16:43:17 +0700

    return xmlrpc func

--- conference/views.py
+++ conference/views.py
@@ -4,6 +4,7 @@ from lxml import objectify as O
 from xmlrpclib import ServerProxy
 from django.shortcuts import render_to_response, get_object_or_404
 from django.http import HttpResponseRedirect
+import syslog
 
 def fsapi(*args,**kwargs):
     server = ServerProxy("http://freeswitch:works@127.0.0.1:8080")
@@ -12,8 +13,7 @@ def fsapi(*args,**kwargs):
 def list(request,do=None,id=None,cnf=None):
     r = O.fromstring(fsapi("conference","xml_list"))
     if do and id:
-        #print do,id,cnf
-        #print 'FS:',fsapi("conference","%s %s %s"%(cnf,do,id))
+        fsapi("conference","%s %s %s"%(cnf,do,id))
         return HttpResponseRedirect('/confs/')
 
     if r.countchildren() > 0:
