b855af938bc6ff2d76ef642d1f1459dfd7d19ffc
krivushinme <mkrivushin@yandex.ru>
2009-06-04 15:43:26 +0700

    Add inviting

--- conference/views.py
+++ conference/views.py
@@ -1,10 +1,14 @@
-# Create your views here.
-#import xml.etree.ElementTree as ET
 from lxml import objectify as O
 from xmlrpclib import ServerProxy
 from django.shortcuts import render_to_response, get_object_or_404
 from django.http import HttpResponseRedirect
+from django import forms
+from django.conf import settings
 import syslog
+
+class InviteParticipantFrom(forms.Form):
+    conference = forms.CharField(widget=forms.widgets.HiddenInput())
+    phone = forms.CharField()
 
 def fsapi(*args,**kwargs):
     server = ServerProxy("http://freeswitch:works@127.0.0.1:8080")
@@ -16,10 +20,18 @@ def list(request,do=None,id=None,cnf=None):
         fsapi("conference","%s %s %s"%(cnf,do,id))
         return HttpResponseRedirect('/confs/')
 
+    if request.method == 'POST':
+        form = InviteParticipantFrom(request.POST)
+        if form.is_valid():
+            conf = form.cleaned_data.get('conference')
+            phone = form.cleaned_data.get('phone')
+            fsapi("bgapi","conference %s@default dial "%conf + settings.DIALTEMPLATE%phone)
+
     if r.countchildren() > 0:
         conferences = r.conference
         members = [[conf.get('name'),
-            [m for m in conf.members.member]] for conf in conferences]
+            [m for m in conf.members.member],InviteParticipantFrom({'conference':conf.get('name')})] 
+                for conf in conferences]
     else:
         members = []
     return render_to_response('confrences.html',{'confs':members})
