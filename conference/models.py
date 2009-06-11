feec0cb4c3ed4623e27410dad3f1fa7078378dfd
krivushinme <mkrivushin@yandex.ru>
2009-06-11 15:17:19 +0700

    Add some conference staff - now you can reinvite participant

--- conference/models.py
+++ conference/models.py
@@ -1,24 +1,30 @@
 from django.db import models as m
 from django.conf import settings
-from views import fsapi 
+from fsapi import *
 
 # Create your models here.
 
 class Conference(m.Model):
     name = m.CharField("Name",max_length=254)
-    number = m.IntegerField("Number",max_length=254)
+    number = m.CharField("Number",max_length=254)
     pin = m.IntegerField("Password",max_length=254,blank=True,null=True)
     participants = m.ManyToManyField("Phone")
     def start(self):
         participants = list(self.participants.all())
         if participants:
             for p in participants:
-                fsapi("bgapi","conference %s@default dial "%self.number + settings.DIALTEMPLATE%p.number)
+                call_from_conference(self.number,p.number)
     def __unicode__(self):
         return self.name
 
 class Phone(m.Model):
     name = m.CharField("Name",max_length=254)
     number = m.IntegerField("Number",max_length=254)
-    def __unicode__(self):
+    def caller_id_name(self):
         return self.name
+    def caller_id_number(self):
+        return self.number
+    def member(self):
+        return self
+    def __unicode__(self):
+        return "%s %s"%(self.name,self.number)
--- conference/views.py
+++ conference/views.py
@@ -1,39 +1,46 @@
 from lxml import objectify as O
-from xmlrpclib import ServerProxy
 from django.shortcuts import render_to_response, get_object_or_404
 from django.http import HttpResponseRedirect
 from django import forms
 from django.conf import settings
+from models import Conference
+from fsapi import *
 import syslog
 
 class InviteParticipantFrom(forms.Form):
     conference = forms.CharField(widget=forms.widgets.HiddenInput())
     phone = forms.CharField()
 
-def fsapi(*args,**kwargs):
-    server = ServerProxy("http://freeswitch:works@127.0.0.1:8080")
-    return server.freeswitch.api(*args,**kwargs)
-
-def list(request,do=None,id=None,cnf=None):
+def list(request,do="",id="",cnf="",param=""):
     r = O.fromstring(fsapi("conference","xml_list"))
     if do and id:
         fsapi("conference","%s %s %s"%(cnf,do,id))
         return HttpResponseRedirect('/confs/')
 
-    if request.method == 'POST':
-        form = InviteParticipantFrom(request.POST)
+    if request.GET:
+        form = InviteParticipantFrom(request.GET)
         if form.is_valid():
             conf = form.cleaned_data.get('conference')
             phone = form.cleaned_data.get('phone')
-            fsapi("bgapi","conference %s@default dial "%conf + settings.DIALTEMPLATE%phone)
+            call_from_conference(conf,phone)
 
+    conferences = []
     if r.countchildren() > 0:
-        conferences = r.conference
-        members = [[conf.get('name'),
-            [m for m in conf.members.member],InviteParticipantFrom({'conference':conf.get('name')})] 
-                for conf in conferences]
-    else:
-        members = []
-    return render_to_response('confrences.html',{'confs':members})
+        for conf in r.conference:
+            conf_name =conf.get('name') 
+            conference = Conference.objects.filter(number=conf_name)
+            members = dict([(str(m.number),m) for m in conference[0].participants.all()]) if conference else {}
+            fs_members = [m for m in conf.members.member]
+            for fm in fs_members:
+                if str(fm.caller_id_name) in members:
+                    fm.member = members[str(fm.caller_id_name)]
+                    del members[str(fm.caller_id_name)]
+                else:
+                    fm.member = None
+            fs_members.extend(members.values())
+            conf_res = [conf_name, fs_members,InviteParticipantFrom({'conference':conf.get('name')}),
+            conference[0] if conference else None] 
+            conferences.append(conf_res)
+    return render_to_response('confrences.html',{'confs':conferences})
 
 
